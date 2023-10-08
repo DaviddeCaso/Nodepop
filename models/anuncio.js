630d9a549081169224193142c7a34ac790970442
David de Caso <david.ddcg@gmail.com>
2023-10-08 17:10:51 +0200

    Defino modelo con Mongoose.

---
+++ models/anuncio.js
@@ -0,0 +1,13 @@
+// models/anuncio.js
+
+const mongoose = require('mongoose');
+
+const anuncioSchema = new mongoose.Schema({
+  nombre: String,
+  venta: Boolean,
+  precio: Number,
+  foto: String,
+  tags: [String],
+});
+
+module.exports = mongoose.model('Anuncio', anuncioSchema);
